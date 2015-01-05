c7ec631223131faedc69927c8c9d9d0047b2bc34
Elias G. Schneevoigt <eliasgs@gmail.com>
2015-01-05 14:12:14 +0100

    Refactor tests

--- test/default.js
+++ test/default.js
@@ -1,125 +1,51 @@
 var test = require('tap').test;
-var mongo = require('mongodb').MongoClient;
-var QueryCache = require('../');
+var Store = require('../datastores/default');
 
-var DB   = process.env.QC_TEST_DB || 'test';
-var URI  = process.env.QC_URI || 'mongodb://127.0.0.1:27017';
+test('Honour the MAX_ENTRIES bound', function (t) {
+  var maxEntries = 2;
+  var cache = new Store({maxEntries: maxEntries});
+  var entries;
+  for (var i=1;i<5;i++) {
+    cache.set(i, i);
+    entries = Object.keys(cache._cache).length;
+    if (i>maxEntries) {
+      t.equal(entries, maxEntries, 'Correct number of entries: ' + maxEntries);
+    } else {
+      t.equal(entries, i, 'Correct number of entries: ' + i);
+    }
+  }
+  t.end();
+});
+test('When MAX_ENTRIES is reached, remove oldest entry', function (t) {
+  var cache = new Store({maxEntries: 2});
+  cache.set('key1', 'val1');
+  cache.set('key2', 'val2');
+  cache.set('key3', 'val3');
+  val = cache.get('key1');
+  t.equal(val, undefined, 'Oldest entry removed');
+  val = cache.get('key2');
+  t.equal(val, 'val2', 'Second entry intact');
+  val = cache.get('key3');
+  t.equal(val, 'val3', 'Third entry intact');
+  t.end();
+});
+test('QueryCache#maxEntries is set correctly', function (t) {
+  var cache = new Store();
+  t.equal(cache.maxEntries, 100000, 'Default is 100000');
 
-test('Cache enabled', function (enabled) {
-  QueryCache._eventbus.once('enable', function () {
-    enabled.end();
-    test('Connect to test database', function (conTest) {
-      mongo.connect(URI, function (err, db) {
-        conTest.error(err);
-        conTest.end();
+  process.env.QC_MAX_ENTRIES = '10';
+  delete require.cache[require.resolve('../datastores/default')];
+  Store = require('../datastores/default');
+  cache = new Store();
+  t.equal(cache.maxEntries, 10, 'Can be set by env var');
 
-        db = db.db(DB);
-        test('Basic cache read/write operations', function (t) {
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          var a = qcache.set('some key', 'some val');
-          var b = qcache.get('some key');
-          t.equal(a, 'some val', '#set returns the value');
-          t.equal(b, 'some val', '#get returns the value');
-          t.end();
-        });
-        test('Basic cache invalidations', function (t) {
-          t.plan(2);
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          qcache.set('some key', 'some val');
+  cache = new Store({maxEntries: 20});
+  t.equal(cache.maxEntries, 20, 'Can be set by constructor');
+  t.end();
+});
 
-          QueryCache._eventbus.once(DB + '.test1', function () {
-            t.deepEqual(qcache.cache._cache, {}, 'cache is empty');
-            qcache.set('some key', 'some val');
-            insert('test2');
-          });
-
-          QueryCache._eventbus.once(DB + '.test2', function () {
-            t.deepEqual(qcache.cache._cache, {}, 'cache is empty');
-          });
-
-
-          insert('test1');
-          function insert(colName) {
-            var col = db.collection(colName);
-            col.insert({test: colName}, function (err) {
-              if (err) throw err;
-            });
-          }
-        });
-        test('Honour the MAX_ENTRIES bound', function (t) {
-          var maxEntries = 2;
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2'],
-            maxEntries: maxEntries
-          });
-          var entries;
-          for (var i=1;i<5;i++) {
-            qcache.set(i, i);
-            entries = Object.keys(qcache.cache._cache).length;
-            if (i>maxEntries) {
-              t.equal(entries, maxEntries,
-                      'Correct number of entries: ' + maxEntries);
-            } else {
-              t.equal(entries, i, 'Correct number of entries: ' + i);
-            }
-          }
-          t.end();
-        });
-        test('When MAX_ENTRIES is reached, remove oldest entry', function (t) {
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2'],
-            maxEntries: 2,
-          });
-          qcache.set('key1', 'val1');
-          qcache.set('key2', 'val2');
-          qcache.set('key3', 'val3');
-          val = qcache.get('key1');
-          t.equal(val, undefined, 'Oldest entry removed');
-          val = qcache.get('key2');
-          t.equal(val, 'val2', 'Second entry intact');
-          val = qcache.get('key3');
-          t.equal(val, 'val3', 'Third entry intact');
-          t.end();
-        });
-        test('QueryCache#maxEntries is set correctly', function (t) {
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          t.equal(qcache.cache.maxEntries, 100000, 'Default is 100000');
-
-          process.env.QC_MAX_ENTRIES = '10';
-          delete require.cache[require.resolve('../datastores/default')];
-          QueryCache = require('../');
-          qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          t.equal(qcache.cache.maxEntries, 10, 'Can be set by env var');
-
-          qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2'],
-            maxEntries: 20
-          });
-          t.equal(qcache.cache.maxEntries, 20, 'Can be set by constructor');
-          t.end();
-        });
-
-        test('exit', function (t) {
-          process.exit(0);
-        });
-      });
-    });
-  });
+test('exit', function (t) {
+  process.exit(0);
 });
 
 function skiptest() {};
---
+++ test/index.js
@@ -0,0 +1,63 @@
+var test = require('tap').test;
+var mongo = require('mongodb').MongoClient;
+var QueryCache = require('../');
+
+var DB   = process.env.QC_TEST_DB || 'test';
+var URI  = process.env.QC_URI || 'mongodb://127.0.0.1:27017';
+
+test('Cache enabled', function (enabled) {
+  QueryCache._eventbus.once('enable', function () {
+    enabled.end();
+    test('Connect to test database', function (conTest) {
+      mongo.connect(URI, function (err, db) {
+        conTest.error(err);
+        conTest.end();
+
+        db = db.db(DB);
+        test('Basic cache read/write operations', function (t) {
+          var qcache = new QueryCache({
+            dbName: DB,
+            collections: ['test1', 'test2']
+          });
+          var a = qcache.set('some key', 'some val');
+          var b = qcache.get('some key');
+          t.equal(a, 'some val', '#set returns the value');
+          t.equal(b, 'some val', '#get returns the value');
+          t.end();
+        });
+        test('Basic automatic cache invalidations', function (t) {
+          t.plan(2);
+          var qcache = new QueryCache({
+            dbName: DB,
+            collections: ['test1', 'test2']
+          });
+          qcache.set('some key', 'some val');
+
+          QueryCache._eventbus.once(DB + '.test1', function () {
+            t.equal(qcache.get('some key'), undefined, 'cache is invalidated');
+            qcache.set('some key', 'some val');
+            insert('test2');
+          });
+
+          QueryCache._eventbus.once(DB + '.test2', function () {
+            t.equal(qcache.get('some key'), undefined, 'cache is invalidated');
+          });
+
+
+          insert('test1');
+          function insert(colName) {
+            var col = db.collection(colName);
+            col.insert({test: colName}, function (err) {
+              if (err) throw err;
+            });
+          }
+        });
+        test('exit', function (t) {
+          process.exit(0);
+        });
+      });
+    });
+  });
+});
+
+function skiptest() {};
