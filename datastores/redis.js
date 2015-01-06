ff34740c3b2b3c498d46cbaf9ada9c278a473bd8
Elias G. Schneevoigt <eliasgs@gmail.com>
2015-01-06 13:46:21 +0100

    Add support for redis as datastore

---
+++ datastores/redis.js
@@ -0,0 +1,69 @@
+var redis = require('redis');
+var async = require('async');
+
+var DEFAULT_MAX_ENTRIES = (process.env.QC_MAX_ENTRIES)?
+                          parseInt(process.env.QC_MAX_ENTRIES, 10):
+                          100000;
+
+var PORT = process.env.QC_REDIS_PORT || 6379;
+var HOST = process.env.QC_REDIS_HOST || '127.0.0.1';
+
+module.exports = Store;
+
+function Store(options) {
+  options = options || {};
+
+  // The maximum number of allowed entries in cache can be specified either by
+  // environmental variable QC_MAX_ENTRIES or the maxEntries option to
+  // the constructor, which gets prioritized over QC_MAX_ENTRIES.
+  this.maxEntries = DEFAULT_MAX_ENTRIES;
+  if (options.maxEntries) {
+    this.maxEntries = options.maxEntries;
+  }
+
+  this._cacheKeys = [];
+
+  this.namespace = options.namespace;
+
+  var redisOptions;
+  if (options.config && options.config.redis) {
+    redisOptions = options.config.redis;
+  }
+
+  this.client = redis.createClient(PORT, HOST, redisOptions);
+}
+
+Store.prototype.invalidate = function (callback) {
+  var self = this;
+
+  self._cacheKeys = [];
+  self.client.keys(self.namespace + '*', function (err, keys) {
+    if (err) return callback(err);
+    async.each(keys, function (key, cb) {
+      self.client.del(key, cb);
+    }, callback);
+  });
+};
+
+Store.prototype.get = function (key, callback) {
+  var _key = this.namespace + '::' + JSON.stringify(key);
+  this.client.get(_key, callback);
+};
+
+Store.prototype.set = function (key, value, callback) {
+  var self = this;
+
+  var _key = self.namespace + '::' + JSON.stringify(key);
+
+  if (self._cacheKeys.length >= self.maxEntries) {
+    self.client.del(self._cacheKeys.shift(), save);
+  } else {
+    save();
+  }
+
+  function save(err) {
+    if (err) return callback(err);
+    self._cacheKeys.push(_key);
+    self.client.set(_key, value, callback);
+  }
+};
---
+++ test/redis.js
@@ -0,0 +1,66 @@
+var test = require('tap').test;
+var async = require('async');
+var Store = require('../datastores/redis');
+
+var NS = 'querycache_test';
+
+test('Honour the MAX_ENTRIES bound', function (t) {
+  var maxEntries = 2;
+  var cache = new Store({maxEntries: maxEntries, namespace: NS});
+
+  var setCache = [];
+  for (var i=1;i<5;i++) {
+    setCache.push((function (i) {
+      return function (next) {
+        cache.set(i, i, next);
+      };
+    })(i));
+  }
+  async.series(setCache, function (err) {
+    if (err) throw err;
+    var entries = cache._cacheKeys.length;
+    t.equal(entries, maxEntries, 'Correct number of entries: ' + maxEntries);
+    t.end();
+  });
+});
+test('When MAX_ENTRIES is reached, remove oldest entry', function (t) {
+  var cache = new Store({maxEntries: 2, namespace: NS});
+  async.series([
+    cache.set.bind(cache, 'key1', 'val1'),
+    cache.set.bind(cache, 'key2', 'val2'),
+    cache.set.bind(cache, 'key3', 'val3')], allSet);
+  function allSet(err) {
+    if (err) throw err;
+    async.series([
+      cache.get.bind(cache, 'key1'),
+      cache.get.bind(cache, 'key2'),
+      cache.get.bind(cache, 'key3')], done);
+    function done(err, values) {
+      if (err) throw err;
+      t.equal(values[0], null, 'Oldest entry removed');
+      t.equal(values[1], 'val2', 'Second entry intact');
+      t.equal(values[2], 'val3', 'Third entry intact');
+      t.end();
+    }
+  }
+});
+test('QueryCache#maxEntries is set correctly', function (t) {
+  var cache = new Store();
+  t.equal(cache.maxEntries, 100000, 'Default is 100000');
+
+  process.env.QC_MAX_ENTRIES = '10';
+  delete require.cache[require.resolve('../datastores/default')];
+  Store = require('../datastores/default');
+  cache = new Store();
+  t.equal(cache.maxEntries, 10, 'Can be set by env var');
+
+  cache = new Store({maxEntries: 20});
+  t.equal(cache.maxEntries, 20, 'Can be set by constructor');
+  t.end();
+});
+
+test('exit', function (t) {
+  process.exit(0);
+});
+
+function skiptest() {};
