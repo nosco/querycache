9fe66d068c3192abb3fccdf2d3319bc6425bbf11
Elias G. Schneevoigt <eliasgs@gmail.com>
2015-01-07 09:44:59 +0100

    Clean up test and a tests for disabled querycache

--- test/index.js
+++ test/index.js
@@ -5,72 +5,90 @@ var QueryCache = require('../');
 var DB   = process.env.QC_TEST_DB || 'test';
 var URI  = process.env.QC_URI || 'mongodb://127.0.0.1:27017';
 
+var db;
+
 test('Cache enabled', function (enabled) {
+  enabled.plan(1);
   QueryCache._eventbus.once('enable', function () {
-    enabled.end();
-    test('Connect to test database', function (conTest) {
-      mongo.connect(URI, function (err, db) {
-        conTest.error(err);
-        conTest.end();
+    enabled.ok(true, 'event fired');
+  });
+});
 
-        db = db.db(DB);
-        test('Basic cache read/write operations', function (t) {
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          qcache.set('some key', 'some val', function (err, a) {
-            t.error(err);
-            qcache.get('some key', function (err, b) {
-              t.error(err);
-              t.equal(a, 'some val', '#set returns the value');
-              t.equal(b, 'some val', '#get returns the value');
-              t.end();
-            });
-          });
-        });
-        test('Basic automatic cache invalidations', function (t) {
-          t.plan(2);
-          var qcache = new QueryCache({
-            dbName: DB,
-            collections: ['test1', 'test2']
-          });
-          qcache.set('some key', 'some val', function (err) {
-            if (err) throw err;
-          });
+test('Connect to test database', function (conTest) {
+  mongo.connect(URI, function (err, _db) {
+    conTest.plan(1);
+    conTest.error(err, 'connected');
 
-          QueryCache._eventbus.once(DB + '.test1', function () {
-            qcache.get('some key', function (err, val) {
-              if (err) throw err;
-              t.equal(val, null, 'cache is invalidated');
-              qcache.set('some key', 'some val', function (err) {
-                t.error(err);
-                insert('test2');
-              });
-            });
-          });
+    db = _db.db(DB);
+  });
+});
 
-          QueryCache._eventbus.once(DB + '.test2', function () {
-            qcache.get('some key', function (err, val) {
-              if (err) throw err;
-              t.equal(val, null, 'cache is invalidated');
-            });
-          });
-
-          insert('test1');
-          function insert(colName) {
-            var col = db.collection(colName);
-            col.insert({test: colName}, function (err) {
-              if (err) throw err;
-            });
-          }
-        });
-        test('exit', function (t) {
-          process.exit(0);
-        });
-      });
+test('Basic cache read/write operations', function (t) {
+  t.plan(4);
+  var qcache = new QueryCache({
+    dbName: DB,
+    collections: ['test1', 'test2']
+  });
+  qcache.set('some key', 'some val', function (err, a) {
+    t.error(err, 'set finished with no error');
+    qcache.get('some key', function (err, b) {
+      t.error(err, 'get finished with no error');
+      t.equal(a, 'some val', 'set returns the value');
+      t.equal(b, 'some val', 'get returns the value');
     });
   });
 });
 
-function skiptest() {};
+test('Basic automatic cache invalidations', function (t) {
+  t.plan(4);
+  var qcache = new QueryCache({
+    dbName: DB,
+    collections: ['test1', 'test2']
+  });
+  QueryCache._eventbus.once(DB + '.test1', function () {
+    qcache.get('some key', function (err, val) {
+      if (err) throw err;
+      t.equal(val, null, 'cache is invalidated');
+      qcache.set('some key', 'some val', function (err) {
+        t.error(err, 'set finished with no error');
+        insert('test2');
+      });
+    });
+  });
+
+  QueryCache._eventbus.once(DB + '.test2', function () {
+    qcache.get('some key', function (err, val) {
+      if (err) throw err;
+      t.equal(val, null, 'cache is invalidated');
+    });
+  });
+
+  qcache.set('some key', 'some val', function (err) {
+    if (err) throw err;
+    insert('test1');
+  });
+
+  function insert(colName) {
+    var col = db.collection(colName);
+    col.insert({test: colName}, function (err) {
+      t.error(err, 'test doc inserted');
+    });
+  }
+});
+
+test('Disabled QueryCache', function (disabled) {
+  disabled.plan(3);
+  var qcache = new QueryCache({dbName: DB, collections: ['test']});
+  qcache.disable();
+  qcache.set('some key', 'some val', function (err) {
+    disabled.error(err, 'set finished with no error');
+    qcache.get('some key', function (err, value) {
+      disabled.error(err, 'get finished with no error');
+      disabled.equal(value, undefined, 'got no value');
+    });
+  });
+});
+
+test('exit', function () {
+  process.exit(0);
+});
